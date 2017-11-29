ff25e2dba3cbf5135762c287a63f4387ec4ec253
Alexandr Volontyr <avolontyr@alteryx.com>
2017-11-29 10:49:21 +0200

    Lesson-8. Tasks (Alexandr Volontyr)

--- src/lesson-8/well-known-symbols/example-5.js
+++ src/lesson-8/well-known-symbols/example-5.js
@@ -4,8 +4,21 @@ function Temperature(degrees) {
     this.degrees = degrees;
 }
 
+Temperature.prototype[Symbol.toPrimitive] = function(hint) {
+    switch (hint) {
+        case 'string':
+            return `${this.degrees}\u00b0`;
+        case 'number':
+            return this.degrees;
+        default:
+            return `${this.degrees} degrees`;
+    }
+};
+
 const freezing = new Temperature(32);
 
 console.log(freezing + '!'); // [object Object]! → // 32 degrees!
 console.log(freezing / 2); // NaN → // 16
 console.log(String(freezing)); // [object Object] → // 32°
+
+const a = 2;
