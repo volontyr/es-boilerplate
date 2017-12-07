3f2a95d9cebd787771faa349ae99936ac92ca1ed
Alexandr Volontyr <avolontyr@alteryx.com>
2017-12-07 12:18:13 +0200

    Lesson-11. Tasks (Alexandr Volontyr)

--- src/lesson-11/tasks/example-2.js
+++ src/lesson-11/tasks/example-2.js
@@ -1,12 +1,36 @@
 import fetch from 'isomorphic-fetch';
 
-fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=886faf57284140dd830bf5d6ddadd32f')
-    .then(function(response) {
-        if (response.status >= 400) {
-            throw new Error("Bad response from server");
-        }
-        return response.json();
-    })
-    .then(function(stories) {
-        console.log(stories);
+const urls = [
+    'https://newsapi.org/v2/sources?country=us&apiKey=93fc45b5eee342f9bd05d81c87558c9b',
+    'https://newsapi.org/v2/sources?country=fr&apiKey=93fc45b5eee342f9bd05d81c87558c9b',
+    'https://newsapi.org/v2/sources?country=it&apiKey=93fc45b5eee342f9bd05d81c87558c9b'
+];
+
+const myFetch = (url) => {
+    return fetch(url)
+        .then((response) => {
+            if (response.status >= 400) {
+                throw new Error(`Bad response from server while fetching ${ response.url }`);
+            }
+            return response.json();
+        })
+        .then(({ sources }) => {
+            const result = [];
+            for (const {id, description} of sources) {
+                result.push({id, description});
+            }
+            return result;
+        })
+        .catch((error) => {
+            console.log(`Could not retrieve needed information. Cause: ${ error.message }`);
+            return [];
+        });
+};
+
+Promise.all(urls.map(url => myFetch(url)))
+    .then((result) => {
+        const data = result.reduce((arr, item) => {
+            return [...arr, ...item];
+        }, []);
+        console.log(`Fetched data = ${ JSON.stringify(data, null, 4) }`);
     });
